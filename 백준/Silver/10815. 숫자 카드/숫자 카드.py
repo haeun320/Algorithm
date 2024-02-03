5b34caf60a5faa2d7c0d39b53b8dc74aa8740d8e
최하은 <121525026+haeun320@users.noreply.github.com>
2024-02-03 23:38:01 +0900

    [Silver V] Title: 숫자 카드, Time: 724 ms, Memory: 125184 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,24 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(array, target, start, end):
-  while (start <= end):
-    mid = (start + end) // 2
-    
-    if array[mid] == target:
-      return 1
-    elif array[mid] > target:
-      end = mid - 1
-    else:
-      start = mid + 1
-  return 0
-
 N = int(input().rstrip())
-have = list(map(int, input().split()))
+have = set(map(int, input().split()))
 M = int(input().rstrip())
 guess = list(map(int, input().split()))
 
-have.sort()
-
 for i in guess:
-  print(binary_search(have, i, 0, N-1), end = " ")
+  if i in have:
+    print(1, end = " ")
+  else:
+    print(0, end = " ")
