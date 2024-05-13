008eb2a1f348b817ce44a059dc7cda9d7b80901c
최하은 <121525026+haeun320@users.noreply.github.com>
2024-05-13 21:46:33 +0900

    [Bronze V] Title: 합, Time: 148 ms, Memory: 9468 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+    rl.close();
+}).on('close', function () {
+    const n = Number(input[0]);
+    let sum = n * (n+1) / 2
+    console.log(sum);
+});
