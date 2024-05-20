0a797d317498c14ea2d33203dbc0341c4858174a
최하은 <121525026+haeun320@users.noreply.github.com>
2024-05-20 21:22:41 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    if (ineq == "<"){
+        if (eq == "=")
+            return Number(n <= m);
+        else 
+            return Number(n < m);
+    }
+    else {
+        if (eq == "=")
+            return Number(n >= m);
+        else 
+            return Number(n > m);
+    }
+}
