40ec52a54cef977e037157b85d702a1f45c0da2b
최하은 <121525026+haeun320@users.noreply.github.com>
2024-06-14 15:30:23 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 9.09 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,11 +1,7 @@
 function solution(arr, queries) {
-    for (var i = 0; i < queries.length; i++){
-        var pos1 = queries[i][0];
-        var pos2 = queries[i][1];
-        
-        var tmp = arr[pos1];
-        arr[pos1] = arr[pos2];
-        arr[pos2] = tmp;
-    }
-    return arr
+    queries.forEach(function([a,b]){
+        console.log(a,b);
+        [arr[a], arr[b]] = [arr[b], arr[a]];
+    });
+    return arr;
 }
