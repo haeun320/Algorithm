c2e0c6002aaba42a0f4e6ecc6b39181d73e4c435
최하은 <121525026+haeun320@users.noreply.github.com>
2024-06-14 17:29:10 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.34 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,15 +1,13 @@
 function solution(arr, queries) {
     var answer = [];
     queries.forEach(function([s, e, k]){
-        var min = 1000000, flag = 0;
-        
+        var min = -1;
         for (var i = s; i <= e; i++){
-            if (arr[i] > k && arr[i] < min){
-                min = arr[i];
-                flag = 1;
-            }
+            if (arr[i] <= k)
+                continue;
+            min = (min == -1 ? arr[i] : Math.min(min, arr[i]));
         }
-        answer.push(flag == 0 ? -1 : min);
+        answer.push(min);
     });
     return answer;
 }
