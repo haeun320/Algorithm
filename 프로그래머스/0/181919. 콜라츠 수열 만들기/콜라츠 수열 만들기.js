ee5119765a2eeafae29553bc2476a1c9ca5c28e8
최하은 <121525026+haeun320@users.noreply.github.com>
2024-06-20 14:36:35 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,12 +1,9 @@
-function solution(n) {
-    var answer = [];
-    while(n != 1){
-        answer.push(n);
-        if (n % 2 == 0)
-            n = n/2;
-        else
-            n = 3 * n + 1;
-    }
-    answer.push(n);
-    return answer;
+function solution(n, arr=[]){
+    arr.push(n);
+    if (n == 1)
+        return arr;
+    else if (n%2 == 0)
+        return solution(n/2, arr);
+    else
+        return solution(3*n+1, arr);
 }
