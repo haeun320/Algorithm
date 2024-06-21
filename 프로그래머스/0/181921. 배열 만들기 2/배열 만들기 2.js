6368f39fed05b1d279b76d03e092848526ba68b8
최하은 <121525026+haeun320@users.noreply.github.com>
2024-06-21 15:20:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 11.09 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -2,19 +2,11 @@ function solution(l, r) {
     answer = [-1];
     for (var i = l; i <= r; i++){
         numbers = [...i.toString()];
-        var flag = 1;
-        
-        numbers.forEach(num => {
-            if (num != '5' && num != '0')
-                flag = 0;
-        });
-        
-        if(flag == 1)
+        var flag = numbers.every(num => num == '5' || num == '0');
+    
+        if(flag)
             answer.push(i);
     }
     
-    if (answer.length > 1)
-        answer.shift();
-    
-    return answer;
+    return answer.length > 1 ? answer.slice(1) : answer;
 }
