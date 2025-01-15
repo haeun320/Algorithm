8a1e161045e7c02fb28c7002d36c4a9c0ae89f97
최하은 <121525026+haeun320@users.noreply.github.com>
2025-01-15 14:36:57 +0900

    [level 1] Title: 모의고사, Time: 5.84 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,19 +3,19 @@ def solution(answers):
     
     answerLen = len(answers)
     
-    for i in range(answerLen):
+    for idx, answer in enumerate(answers):
         # student1  
-        if (answers[i] == i % 5 + 1):
+        if (answer == idx % 5 + 1):
             students[0] += 1
             
         # student2
         student2patters = [2,1,2,3,2,4,2,5]
-        if (answers[i] == student2patters[i%8]):
+        if (answer == student2patters[idx%8]):
             students[1] += 1
         
         # student3
         student3patters = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-        if (answers[i] == student3patters[i%10]):
+        if (answer == student3patters[idx%10]):
             students[2] += 1
         
     maxValue = max(students)
