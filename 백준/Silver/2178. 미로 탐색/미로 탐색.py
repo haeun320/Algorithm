2b42a6d2fe8aa44ad7cf51facd557d7cda84b4c8
최하은 <121525026+haeun320@users.noreply.github.com>
2025-06-01 15:35:58 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,35 +1,31 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-
 N, M = map(int, input().split())
-miro = [list(map(int, input().rstrip())) for _ in range(N)]
+maze = [list(map(int, input())) for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+
 def bfs(x, y):
-  q = deque()
-  q.append((x, y))
-  
-  while q:
-    x, y = q.popleft()
-    
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      
-      if nx < 0 or nx >= N or ny < 0 or ny >= M:
-        continue
-      
-      if miro[nx][ny] == 0:
-        continue
-      
-      if miro[nx][ny] == 1:
-        miro[nx][ny] = miro[x][y] + 1
-        q.append((nx, ny))
-        
-  return miro[N-1][M-1]
+    q = deque()
+    q.append((x,y))
+
+    while q:
+        vx, vy = q.popleft()
+
+        for i in range(4):
+            nx = vx + dx[i]
+            ny = vy + dy[i]
+
+            if (nx < 0 or nx >= M or ny < 0 or ny >= N):
+                continue
+            elif (maze[ny][nx] == 0):
+                continue
+            elif (maze[ny][nx] == 1):
+                maze[ny][nx] = maze[vy][vx] + 1
+                q.append((nx, ny))
+
+    return maze[N - 1][M - 1]
 
 print(bfs(0,0))
