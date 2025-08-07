5005d32610ca9723e0add83b7eaf6736cd52e143
최하은 <121525026+haeun320@users.noreply.github.com>
2025-08-07 10:09:22 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll func(ll a, ll b, ll c) {
+	if (b == 0) return 1;
+	if (b == 1) return a % c;
+
+	if (b % 2 == 1) {
+		ll v = func(a, (b-1)/2, c);
+		return ((v * v) % c) * a % c;
+	}
+
+	ll v = func(a, b/2, c);
+	return v * v % c;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	ll a, b, c;
+	cin >> a >> b >> c;
+
+	cout << func(a, b, c);
+	return 0;
+}
