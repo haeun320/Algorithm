cef4eb60c5800ec080505eaa592670e27dc90b9d
최하은 <121525026+haeun320@users.noreply.github.com>
2025-10-01 23:05:27 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 112228 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+
+ans = []
+def back(n):
+  if len(ans) == M:
+    print(" ".join(map(str, ans)))
+    return
+  
+  for i in range(n, N+1):
+    ans.append(i)
+    back(i)
+    ans.pop()
+    
+back(1)
