7b382c21cac0fc29907fabfa5d3ed40a7cbe45d2
최하은 <121525026+haeun320@users.noreply.github.com>
2025-10-19 21:47:25 +0900

    [Silver II] Title: 결혼식, Time: 112 ms, Memory: 110816 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.py
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -1,24 +1,3 @@
-from collections import deque
-
-def bfs():
-  q = deque([(1, 0)])
-  visited[1] = True
-  cnt = 0
-  
-  while q:
-    n, l = q.popleft()
-    cnt += 1
-    
-    if l > 1:
-      continue
-    
-    for p in f[n]:
-      if not visited[p]:
-        visited[p] = True
-        q.append((p, l+1))
-  
-  return cnt-1 # 자기자신 제외
-
 n = int(input())
 m = int(input())
 f = [[] for _ in range(n+1)]
@@ -27,9 +6,20 @@ for i in range(m):
   a, b = map(int, input().split())
   f[a].append(b)
   f[b].append(a)
-  
-# print(f)
 
 visited = [False] * (n+1)
-cnt = bfs()
+visited[1] = True
+cnt = 0
+
+for p in f[1]:
+  if not visited[p]:
+    visited[p] = True
+    cnt += 1
+  # print(f"p = {p}, f[p] = {f[p]}")
+  for q in f[p]:
+    if not visited[q]:
+      # print("insert", q)
+      cnt += 1
+      visited[q] = True
+      
 print(cnt)
