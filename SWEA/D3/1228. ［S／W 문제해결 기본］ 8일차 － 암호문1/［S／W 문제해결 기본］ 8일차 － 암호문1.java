2d4bc4b774311bb5bfbc0589aad04f43c790ef47
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-02 14:08:38 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 74 ms, Memory: 22,912 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -1,5 +1,4 @@
 
-
 import java.io.*;
 import java.util.*;
 
@@ -8,34 +7,37 @@ public class Solution
     public static void main(String args[]) throws Exception
     {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
     	
     	for (int t = 1; t <= 10; t++) {
     		int N = Integer.parseInt(br.readLine());
-    		LinkedList<Integer> list = new LinkedList<>();
+    		List<Integer> list = new LinkedList<>();
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     		for (int i = 0; i < N; i++) {
     			list.add(Integer.parseInt(st.nextToken()));
     		}
     		
     		int cnt = Integer.parseInt(br.readLine());
-    		st = new StringTokenizer(br.readLine());
-    		
-    		for (int i = 0; i < cnt; i++) {
-    			String ord = st.nextToken();
-    			int x = Integer.parseInt(st.nextToken());
-    			int y = Integer.parseInt(st.nextToken());
-    			for (int j = 0; j < y; j++) {
-    				int s = Integer.parseInt(st.nextToken());
-    				list.add(x++, s);
-    			}
-    		}
-    		
-        	System.out.print("#"+t);
+    		st = new StringTokenizer(br.readLine(), " ");
+    		 
+            for (int i = 0; i < cnt; i++) {
+                String ord = st.nextToken();
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                for (int j = 0; j < y; j++) {
+                    int s = Integer.parseInt(st.nextToken());
+                    list.add(x++, s);
+                }
+            }
+             
+    		sb.append("#").append(t);
         	for (int i = 0; i < 10; i++) {
-        		System.out.print(" "+list.get(i));
+        		sb.append(" ").append(list.get(i));
         	}
-        	System.out.println();
+        	sb.append("\n");
     	}
+    	
+    	System.out.println(sb);
     }
 }
