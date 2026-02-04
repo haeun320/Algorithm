6820b10f99c933732a0acd62c98c2e83e8547bb3
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-04 14:36:13 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 119 ms, Memory: 35,968 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+	public static String[][] map = new String[100][100];
+	public static int start;
+	public static int[] end;
+	public static int[] dr = {0, 0, -1};
+	public static int[] dc = {-1, 1, 0};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 0; t < 10; t++) {
+			String tc = br.readLine();
+			
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = st.nextToken();
+					if (i == 99 && map[i][j].equals("2")) {
+						end = new int[] {i, j};
+					}
+				}
+			}
+			
+			// 도착점에 대응되는 출발점 찾기
+			dfs(end[0], end[1]);
+			sb.append("#").append(tc).append(" ").append(start).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int r, int c) {
+		if (r == 0) {
+			start = c;
+			return;
+		}
+		map[r][c] = "0";
+		for (int i = 0; i < 3; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			
+			if (nr < 0 || nr >= 100 || nc < 0 || nc >= 100)
+				continue;
+			
+			if (map[nr][nc].equals("1")) {
+				dfs(nr, nc);
+				break;
+			}
+		}
+	}
+}
