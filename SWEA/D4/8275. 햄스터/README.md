2c89ff460a3d3edeba8f5e56002d1ecf49bda344
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-05 13:32:49 +0900

    [D4] Title: 햄스터, Time: 549 ms, Memory: 28,876 KB -BaekjoonHub

---
+++ SWEA/D4/8275. 햄스터/README.md
@@ -0,0 +1,15 @@
+# [D4] 햄스터 - 8275 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWxQ310aOlQDFAWL) 
+
+### 성능 요약
+
+메모리: 28,876 KB, 시간: 549 ms, 코드길이: 2,294 Bytes
+
+### 제출 일자
+
+2026-02-05 13:32
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+ 
+public class Solution
+{
+	public static int[] hm; // 현재 햄스터 배치
+	public static int[][] arr; // 기록
+	public static int[] result; // 최종 햄스터 배치
+	public static int sum;
+	public static int N, M, X;
+	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        	N = Integer.parseInt(st.nextToken());
+        	X = Integer.parseInt(st.nextToken());
+        	M = Integer.parseInt(st.nextToken());
+        	
+        	hm = new int[N+1];
+        	arr = new int[M][3];
+        	
+        	for (int i = 0; i < M; i++) {
+        		st = new StringTokenizer(br.readLine(), " ");
+        		for (int j = 0; j < 3; j++) {
+        			arr[i][j] = Integer.parseInt(st.nextToken());
+        		}
+        	}
+        	sb.append("#").append(t);
+        	sum = 0;
+        	result = null;
+        	bt(1);
+        	if (result == null) {
+        		sb.append(" ").append("-1").append("\n");
+        		continue;
+        	}
+        	
+        	for (int i = 1; i < N+1; i++) {
+        		sb.append(" ").append(result[i]);
+        	}
+        	sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+    // 만족하는 햄스터 배치가 있으면 true 반환
+    // 합이 가장 큰 순이기 때문에 뒤에서부터 탐색
+    public static void bt(int idx) {
+    	if (idx == N+1) {
+    		if (isPossible()) {
+    			int s = 0;
+    			for (int i = 1; i <= N; i++) {
+    				s += hm[i];
+    			}
+    			if (s >= sum) {
+    				sum = s;
+    				result = hm.clone();
+    			}
+    		}
+    		return;
+    	}
+    	
+    	for (int i = X; i >= 0; i--) { // 각 우리에 0~X마리 햄스터 가능
+    		hm[idx] = i;
+    		bt(idx+1);
+    	}
+    }
+    
+    public static boolean isPossible() {
+    	for (int i = 0; i < M; i++) {
+    		int l = arr[i][0];
+    		int r = arr[i][1];
+    		int s = arr[i][2];
+    		
+    		int sum = 0;
+    		for (int j = l; j <= r; j++) {
+    			sum += hm[j];
+    		}
+    		
+    		if (sum != s) return false;
+    	}
+    	return true;
+    }
+    
+}
