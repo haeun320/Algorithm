9009921d12076079dca41d9afb76bda8e5112a77
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-12 14:40:51 +0900

    [D4] Title: 정사각형 방, Time: 660 ms, Memory: 96,308 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution
+{
+	private static int N, rCnt, rNum; 
+	private static int[][] map;
+	private static int[] dr = {-1, 1, 0, 0};
+	private static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+			
+			rCnt = Integer.MIN_VALUE; // 이동할 수 있는 방 최대 개수
+			rNum = Integer.MAX_VALUE; // 이동할 수 있는 방의 개수가 최대일 때의 방 번호
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					move(i, j);
+				}
+			}
+			
+			sb.append("#").append(tc).append(" ").append(rNum).append(" ").append(rCnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static void move(int r, int c) {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {r, c, 1}); // 좌표 r, c, 노드 수
+		int cnt = 0; // 이동한 방의 수
+		
+		while (!q.isEmpty()) {
+			int[] pos = q.poll();
+			cnt = Math.max(cnt, pos[2]);
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = pos[0] + dr[i];
+				int nc = pos[1] + dc[i];
+				
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N)
+					continue;
+				
+				if (map[nr][nc] == map[pos[0]][pos[1]]+1) {
+					q.add(new int[] {nr, nc, pos[2]+1});
+				}
+			}
+		}
+		
+		if (cnt > rCnt) {
+			rCnt = cnt;
+			rNum = map[r][c];
+		}
+		else if (cnt == rCnt && rNum > map[r][c]) {
+			rNum = map[r][c];
+		}
+	}
+} 
