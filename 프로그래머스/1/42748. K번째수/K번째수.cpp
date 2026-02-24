049bf0b7bfd92c7292fa9222f9463371c56ebdb9
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-24 11:20:44 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -4,15 +4,12 @@ using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
+    answer.reserve(commands.size());
     
-    for (int t = 0; t < commands.size(); t++){
-        int i = commands[t][0];
-        int j = commands[t][1];
-        int k = commands[t][2];
-        
-        vector<int> tmp(array.begin() + (i-1), array.begin() + j);
-        sort(tmp.begin(), tmp.end());
-        answer.push_back(tmp[k-1]);
+    for(const auto& cmd : commands){
+        vector<int> tmp(array.begin() + cmd[0] - 1, array.begin() + cmd[1]);
+        nth_element(tmp.begin(), tmp.begin() + cmd[2] - 1, tmp.end());
+        answer.push_back(tmp[cmd[2] - 1]);
     }
     
     return answer;
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 3.65 MB, 시간: 0.01 ms
+메모리: 4.16 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 24일 11:02:32
+2026년 02월 24일 11:20:42
 
 ### 문제 설명
 
