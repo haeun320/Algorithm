0c789536578968c4d337477080a3e3901f236c55
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-24 11:58:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 120.14 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -8,15 +8,10 @@ vector<int> solution(vector<int> arr)
     vector<int> answer;
     answer.reserve(arr.size());
 
-    for (int i = 0; i < arr.size(); i++){
-        if (i == 0){
-            answer.push_back(arr[i]);
-            continue;
+    for (int num: arr){
+        if (answer.empty() || answer.back() != num){
+            answer.push_back(num);
         }
-        if (arr[i-1] == arr[i]){
-            continue;
-        }
-        answer.push_back(arr[i]);
     }
 
     return answer;
