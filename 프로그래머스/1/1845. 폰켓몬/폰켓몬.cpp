6a2927397727af852bfb23986889252a5ee2b775
최하은 <121525026+haeun320@users.noreply.github.com>
2026-02-24 12:10:03 +0900

    [level 1] Title: 폰켓몬, Time: 1.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -3,10 +3,6 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    set<int> s;
-    for (const auto& n: nums){
-        s.insert(n);
-    }
-    
+    set<int> s(nums.begin(), nums.end());
     return min(nums.size()/2, s.size());
 }
